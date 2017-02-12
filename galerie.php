9e631819f75c1607e5fb89e7b12ba2f2f97d1cfc
NordineKhelfi <nordine.khelfi@gmail.com>
2017-02-12 11:40:18 +0100

    Galerie
    Seperation added between pictures and videos on the "gelerie" page

--- galerie.php
+++ galerie.php
@@ -7,6 +7,24 @@
 </div>
 
 <div class="container">
+
+  <div class="row" style="margin-bottom: 20px;">
+    <form name="select" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+      <!-- Select Year -->
+      <div class="form-group">
+        <!-- <label class="col-md-4 control-label" for="selectbasic">Année</label> -->
+        <div class="col-md-4 col-md-offset-4">
+          <select id="selectyear" name="selectyear" class="form-control" onChange='this.form.submit()'>
+            <option value="" disabled selected>Choisissez l'année</option>
+            <option value="2017">2017</option>
+            <option value="2016">2016</option>
+            <option value="2015">2015</option>
+          </select>
+        </div>
+      </div>
+    </form>
+  </div>
+
     <div class="row">
 
       <?php include ("bddConnect.php"); ?>
@@ -32,6 +50,13 @@
 
 
      </div>
+</div>
+
+<div class="container-fluid" style="margin-bottom: 20px; background-color: rgba(46, 46, 46, 0.45);">
+  <h1 class="text-center " style="color: rgba(244, 81, 30, 0.82);"> En vidéo</h1>
+</div>
+
+<div class="container">
 
   <div class="row">
 
@@ -50,29 +75,13 @@
       </div> ';
     }
     ?>
-
-     <!-- <div class="col-xs-6 col-sm-4 col-md-2">
-       <a class="fancybox thumbnail custom-thumbnail various fancybox.iframe" rel="gallery1" href="https://www.youtube.com/embed/70l-kEFR26I?autoplay=1">
-         <img src="http://img.youtube.com/vi/70l-kEFR26I/0.jpg" width="150" height="100" />
-       </a>
-     </div> -->
-
    </div>
 
-<form name="select" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-  <!-- Select Year -->
-  <div class="form-group">
-    <!-- <label class="col-md-4 control-label" for="selectbasic">Année</label> -->
-    <div class="col-md-4 col-md-offset-4">
-      <select id="selectyear" name="selectyear" class="form-control" onChange='this.form.submit()'>
-        <option value="" disabled selected>Choisissez l'année</option>
-        <option value="2017">2017</option>
-        <option value="2016">2016</option>
-        <option value="2015">2015</option>
-      </select>
-    </div>
-  </div>
-</form>
+</div>
+
+
+
+
 
 
 <?php include ("footer.php"); ?>
